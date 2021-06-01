6e72bd9f38eeb41a117418b43d821b441a3e9603
vthoopal <71883281+vthoopal@users.noreply.github.com>
2021-06-01 16:31:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+noseX=0;
+noseY=0;
+
+function preload(){
+	clown_nose = loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png');
+}
+
+
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(300, 300);
+  video.hide();
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+	console.log('poseNet initialized');
+}
+
+function gotPoses(results)
+{
+	if(results.length>0)
+	{
+		console.log(results);
+		noseX = results[0].pose.nose.x - 10;
+		noseY = results[0].pose.nose.y -10;
+		console.log("nose x = "+ noseX);
+		console.log("nose y ="+ noseY);
+	}
+}
+
+
+	function draw() {
+	image(video, 0, 0, 300, 300);
+	fill(255, 0, 0);
+	stroke(255,0,0);
+	image(clown_nose, noseX, noseY, 30,30);
+	}
+
+
+
+	function take_snapshot(){
+		save('myFilterImage.png');
+	}
